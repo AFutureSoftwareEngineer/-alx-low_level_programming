86c2c0ca589339ab9bc549809177018d0fb6e6e6
AFutureSoftwareEngineer <saraelbouchkeri@gmail.com>
2023-03-16 17:12:05 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry point of any C program
+*
+* Return:  specify a value to be returned
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
