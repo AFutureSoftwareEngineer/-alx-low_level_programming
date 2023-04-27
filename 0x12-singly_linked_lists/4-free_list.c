71f31ed0f1e8a3577f842bd1c838cb23f096fa21
AFutureSoftwareEngineer <saraelbouchkeri@gmail.com>
2023-04-27 15:29:08 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function.
+ *
+ * @head:list[0] *pointer
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
